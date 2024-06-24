b8335b0bc0ed023ffdf302110bb57a002c0cb073
kaluottah <kaluottahemeka@gmail.com>
2024-06-24 18:51:38 +0100

    pushing presentation

--- cypress/e2e/login_test.cy.js
+++ cypress/e2e/login_test.cy.js
@@ -2,7 +2,7 @@ import InventoryPage from "../pages/InventoryPage";
 import LoginPage from "../pages/LoginPage";
 
 describe('Login Test', () => {
-  it('Valid Login', () => {
+  it('Verify login successful', () => {
     new LoginPage().login(Cypress.env('SAUCE_USERNAME'), Cypress.env('SAUCE_PASSWORD'))
     new InventoryPage().assertPageUrl().logout()
   });
